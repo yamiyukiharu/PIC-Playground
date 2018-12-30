5281f9b80744e55b9c4696fdd6c9fcdec658e163
Hao Ye <haoye94@hotmail.com>
2018-12-30 13:14:42 +0800

    Added writeByte & writeNibble

--- HD44780.c
+++ HD44780.c
@@ -3,35 +3,34 @@
 
 #define DELAY_MULTIPLIER 2
 
+#define HD44780_4_BIT_INTERFACE		0x2
+
 void write_mode_on();
 void read_mode_on();
 void select_instruction_register();
 void select_data_register();
 void enable_pulse();
+void writeNibble(uint8_t value);
+void writeByte(uint8_t value);
 
 void HD44780_init() {
-    __delay_ms(16*DELAY_MULTIPLIER);
+    delay_ms(16*DELAY_MULTIPLIER);
     digitalWrite_DB7(LOW);
     digitalWrite_DB6(LOW);
     digitalWrite_DB5(HIGH);
     digitalWrite_DB4(HIGH);
     select_instruction_register();
     write_mode_on();
-    __delay_us(1*DELAY_MULTIPLIER);
+    delay_us(1*DELAY_MULTIPLIER);
     enable_pulse();
-    __delay_ms(5*DELAY_MULTIPLIER);
+    delay_ms(5*DELAY_MULTIPLIER);
     enable_pulse();
-    __delay_us(110*DELAY_MULTIPLIER);
+    delay_us(110*DELAY_MULTIPLIER);
     enable_pulse();
-    
-    __delay_us(40*DELAY_MULTIPLIER);
+    delay_us(40*DELAY_MULTIPLIER);
+
     //set 4 bit interface
-    digitalWrite_DB7(LOW);
-    digitalWrite_DB6(LOW);
-    digitalWrite_DB5(HIGH);
-    digitalWrite_DB4(LOW);
-    enable_pulse();
-    __delay_us(40*DELAY_MULTIPLIER);
+    writeNibble(HD44780_4_BIT_INTERFACE);
     
     //specify display lines & font
     digitalWrite_DB7(LOW);
@@ -39,13 +38,13 @@ void HD44780_init() {
     digitalWrite_DB5(HIGH);
     digitalWrite_DB4(LOW);
     enable_pulse();
-    __delay_us(40*DELAY_MULTIPLIER);
+    delay_us(40*DELAY_MULTIPLIER);
     digitalWrite_DB7(HIGH);
     digitalWrite_DB6(LOW);
     digitalWrite_DB5(LOW);
     digitalWrite_DB4(LOW);
     enable_pulse();
-    __delay_us(40*DELAY_MULTIPLIER);
+    delay_us(40*DELAY_MULTIPLIER);
     
     //Display Off
     digitalWrite_DB7(LOW);
@@ -53,13 +52,13 @@ void HD44780_init() {
     digitalWrite_DB5(LOW);
     digitalWrite_DB4(LOW);
     enable_pulse();
-    __delay_us(40*DELAY_MULTIPLIER);
+    delay_us(40*DELAY_MULTIPLIER);
     digitalWrite_DB7(HIGH);
     digitalWrite_DB6(LOW);
     digitalWrite_DB5(LOW);
     digitalWrite_DB4(LOW);
     enable_pulse();
-    __delay_us(40*DELAY_MULTIPLIER);
+    delay_us(40*DELAY_MULTIPLIER);
     
     //Display Clear
     digitalWrite_DB7(LOW);
@@ -67,13 +66,27 @@ void HD44780_init() {
     digitalWrite_DB5(LOW);
     digitalWrite_DB4(LOW);
     enable_pulse();
-    __delay_us(40*DELAY_MULTIPLIER);
+    delay_us(40*DELAY_MULTIPLIER);
     digitalWrite_DB7(HIGH);
     digitalWrite_DB6(HIGH);
     digitalWrite_DB5(HIGH);
     digitalWrite_DB4(LOW);
     enable_pulse();
-    __delay_us(40*DELAY_MULTIPLIER);
+    delay_us(40*DELAY_MULTIPLIER);
+}
+
+void writeNibble(uint8_t value) {
+	digitalWrite_DB7(value >> 3 | 0b1);
+    digitalWrite_DB6(value >> 2 | 0b1);
+    digitalWrite_DB5(value >> 1 | 0b1);
+    digitalWrite_DB4(value | 0b1);
+    enable_pulse();
+    delay_us(40*DELAY_MULTIPLIER);
+}
+
+void writeByte(uint8_t value) {
+	writeNibble(value >> 4);
+    writeNibble(value);
 }
 
 void write_mode_on() {
@@ -94,6 +107,6 @@ void select_data_register() {
 
 void enable_pulse() {
     digitalWrite_EN(HIGH);
-    __delay_us(1*DELAY_MULTIPLIER);
+    delay_us(1*DELAY_MULTIPLIER);
     digitalWrite_EN(LOW);
 }
